69a613de1dffc63ab2f7bdd42851c1d88ed8ad81
mobby45 <petitmewen@gmail.com>
2024-06-30 02:14:27 +0200

    Modify it to fix
    Signed-off-by: mobby45 <petitmewen@gmail.com>

--- src/fr/anisama/src/eu/kanade/tachiyomi/animeextension/fr/anisama/AniSama.kt
+++ src/fr/anisama/src/eu/kanade/tachiyomi/animeextension/fr/anisama/AniSama.kt
@@ -40,7 +40,7 @@ class AniSama : ParsedAnimeHttpSource(), ConfigurableAnimeSource {
 
     override val name = "AniSama"
 
-    override val baseUrl = "https://v1.anisama.net"
+    override val baseUrl = "https://animesz.xyz"
 
     override val lang = "fr"
 
