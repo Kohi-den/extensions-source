64d466dfc86e9dbed91457c6dd7ae00e0814532a
Josef František Straka <strakjos@fel.cvut.cz>
2025-01-26 13:52:41 +0100

    fixed yuki subtitles and code cleanup

--- src/en/aniplay/src/eu/kanade/tachiyomi/animeextension/en/aniplay/AniPlayDto.kt
+++ src/en/aniplay/src/eu/kanade/tachiyomi/animeextension/en/aniplay/AniPlayDto.kt
@@ -32,67 +32,11 @@ data class EpisodeData(
 @Serializable
 data class VideoSourceResponse(
     val sources: List<Source>?,
-    val subtitles: List<Subtitle>?,
-) {
-    @Serializable
-    data class Source(
-        val url: String,
-        val quality: String?,
-    )
-
-    @Serializable
-    data class Subtitle(
-        val url: String,
-        val lang: String?,
-    )
-}
-
-// Provider Yuki
-@Serializable
-data class EpisodeDataYuki(
-    val source: String,
-    val language: String,
-    val response: VideoSourceResponseYuki,
-)
-
-@Serializable
-data class VideoSourceResponseYuki(
-    val sources: List<Source>?,
-    val tracks: List<Subtitle>?,
-    val anilistID: Int?,
-    val malID: Int?,
-) {
-    @Serializable
-    data class Source(
-        val url: String,
-        val type: String?,
-    )
-
-    @Serializable
-    data class Subtitle(
-        val file: String,
-        val label: String?,
-        val kind: String?,
-        val default: Boolean?,
-    )
-}
-
-// Provider Pahe
-@Serializable
-data class EpisodeDataPahe(
-    val source: String,
-    val language: String,
-    val response: VideoSourceResponsePahe,
-)
-
-@Serializable
-data class VideoSourceResponsePahe(
-    val sources: List<Source>?,
     val audio: List<Audio>?,
     val intro: Timestamp?,
     val outro: Timestamp?,
     val subtitles: List<Subtitle>?,
-
+    val headers: Headers?,
 ) {
     @Serializable
     data class Source(
@@ -118,6 +62,11 @@ data class VideoSourceResponsePahe(
     data class Subtitle(
         val url: String?,
         val lang: String?,
+    )
+
+    @Serializable
+    data class Headers(
+        val Referer: String?,
     )
 }
 
