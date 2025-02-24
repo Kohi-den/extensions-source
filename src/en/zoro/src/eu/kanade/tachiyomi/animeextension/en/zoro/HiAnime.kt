6a939e4f4a3943f853e47f36eeb79e3d83767948
AlphaBoom <30779939+AlphaBoom@users.noreply.github.com>
2025-02-24 18:54:10 +0800

    Hianime: Remove search ref in anime url. (#708)
    * HiAnime: Remove search ref in anime url.
    
    * Bump Version

--- src/en/zoro/src/eu/kanade/tachiyomi/animeextension/en/zoro/HiAnime.kt
+++ src/en/zoro/src/eu/kanade/tachiyomi/animeextension/en/zoro/HiAnime.kt
@@ -1,11 +1,13 @@
 package eu.kanade.tachiyomi.animeextension.en.zoro
 
+import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.lib.megacloudextractor.MegaCloudExtractor
 import eu.kanade.tachiyomi.lib.streamtapeextractor.StreamTapeExtractor
 import eu.kanade.tachiyomi.multisrc.zorotheme.ZoroTheme
 import eu.kanade.tachiyomi.network.GET
 import okhttp3.Request
+import org.jsoup.nodes.Element
 
 class HiAnime : ZoroTheme(
     "en",
@@ -25,6 +27,12 @@ class HiAnime : ZoroTheme(
     private val megaCloudExtractor by lazy { MegaCloudExtractor(client, headers, preferences) }
 
     override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/recently-updated?page=$page", docHeaders)
+
+    override fun popularAnimeFromElement(element: Element): SAnime {
+        return super.popularAnimeFromElement(element).apply {
+            url = url.substringBefore("?")
+        }
+    }
 
     override fun extractVideo(server: VideoData): List<Video> {
         return when (server.name) {
