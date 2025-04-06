bca227aafcb5e54262d984fe8931f84298d037fc
Thinker <170967310+GraveEaterMadison@users.noreply.github.com>
2025-04-06 10:49:48 +0530

    Update HiAnime.kt

--- src/en/zoro/src/eu/kanade/tachiyomi/animeextension/en/zoro/HiAnime.kt
+++ src/en/zoro/src/eu/kanade/tachiyomi/animeextension/en/zoro/HiAnime.kt
@@ -12,7 +12,7 @@ import org.jsoup.nodes.Element
 class HiAnime : ZoroTheme(
     "en",
     "HiAnime",
-    "https://hianime.bz",
+    "https://hianime.to",
     hosterNames = listOf(
         "HD-1",
         "HD-2",
