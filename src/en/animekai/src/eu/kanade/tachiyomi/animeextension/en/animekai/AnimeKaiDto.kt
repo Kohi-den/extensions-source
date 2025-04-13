82c9b5b182f94b21b6024b6f182495fa1f3982f7
Arkai1 <codinsanemobile@gmail.com>
2025-04-13 21:41:38 +0530

    Update AnimeKaiDto.kt

--- src/en/animekai/src/eu/kanade/tachiyomi/animeextension/en/animekai/AnimeKaiDto.kt
+++ src/en/animekai/src/eu/kanade/tachiyomi/animeextension/en/animekai/AnimeKaiDto.kt
@@ -1,15 +1,33 @@
 package eu.kanade.tachiyomi.animeextension.en.animekai
 
-// DTO for generating token requests
+/**
+ * Data Transfer Object for token request.
+ *
+ * @property id The unique identifier for the token request.
+ * @property time The timestamp for the token request.
+ */
 data class TokenRequestDTO(
     val id: String,
     val time: String,
 )
 
+/**
+ * Data Transfer Object for iframe data.
+ *
+ * @property encryptedData The encrypted data contained in the iframe.
+ */
 data class IframeDataDTO(
     val encryptedData: String,
 )
 
+/**
+ * Data Transfer Object for AnimeKai decoder.
+ *
+ * @property secret The secret key for decoding.
+ * @property iv The initialization vector for decoding.
+ * @property tokenRequest The token request data.
+ * @property iframeData The iframe data to decode.
+ */
 data class AnimekaiDecoderDTO(
     val secret: String = AnimekaiDecoder.SECRET,
     val iv: String = AnimekaiDecoder.IV,
