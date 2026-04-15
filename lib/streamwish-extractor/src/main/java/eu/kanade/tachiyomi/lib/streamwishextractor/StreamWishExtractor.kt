110d66c483bd0f14c5e9d8621861a618d8545a9d
IsmaCortGtz <ismacortgtz@gmail.com>
2026-04-14 20:51:44 -0600

    fix(lib/streamwish-extractor): Fix streamwish-extractor redirections

--- lib/streamwish-extractor/src/main/java/eu/kanade/tachiyomi/lib/streamwishextractor/StreamWishExtractor.kt
+++ lib/streamwish-extractor/src/main/java/eu/kanade/tachiyomi/lib/streamwishextractor/StreamWishExtractor.kt
@@ -5,13 +5,13 @@ import eu.kanade.tachiyomi.animesource.model.Track
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.lib.playlistutils.PlaylistUtils
 import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.util.asJsoup
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
+import org.jsoup.Jsoup
 
 class StreamWishExtractor(private val client: OkHttpClient, private val headers: Headers) {
     private val playlistUtils by lazy { PlaylistUtils(client, headers) }
@@ -21,38 +21,58 @@ class StreamWishExtractor(private val client: OkHttpClient, private val headers:
 
     fun videosFromUrl(url: String, videoNameGen: (String) -> String = { quality -> "StreamWish - $quality" }): List<Video> {
 
-        val doc = client.newCall(GET(getEmbedUrl(url), headers)).execute().asJsoup()
+        val id = getEmbedId(url)
+        for (domain in DOMAINS) {
+            val fullUrl = if (id.startsWith("https://")) id else "https://$domain/$id"
+            try {
 
-        val scriptBody = doc.selectFirst("script:containsData(m3u8)")?.data()
-            ?.let { script ->
-                if (script.contains("eval(function(p,a,c")) {
-                    JsUnpacker.unpackAndCombine(script)
-                } else {
-                    script
+                val response = client.newCall(GET(fullUrl, headers)).execute()
+                if (!response.isSuccessful) continue
+
+                val body = response.body.string()
+                if (body.isEmpty()) continue
+                val doc = Jsoup.parse(body)
+
+                val scriptBody = doc.selectFirst("script:containsData(m3u8)")?.data()
+                    ?.let { script ->
+                        if (script.contains("eval(function(p,a,c")) {
+                            JsUnpacker.unpackAndCombine(script)
+                        } else {
+                            script
+                        }
+                    }
+
+                val masterUrl = scriptBody?.let {
+                    M3U8_REGEX.find(it)?.value
                 }
+
+                if (masterUrl != null) {
+                    val subtitleList = extractSubtitles(scriptBody)
+
+                    return playlistUtils.extractFromHls(
+                        playlistUrl = masterUrl,
+                        referer = "https://${fullUrl.toHttpUrl().host}/",
+                        videoNameGen = videoNameGen,
+                        subtitleList = playlistUtils.fixSubtitles(subtitleList),
+                    )
+                }
+            } catch (e: Exception) {
+                if (id.startsWith("https://")) return emptyList()
             }
-        val masterUrl = scriptBody?.let {
-            M3U8_REGEX.find(it)?.value
         }
-            ?: return emptyList()
 
-        val subtitleList = extractSubtitles(scriptBody)
-
-        return playlistUtils.extractFromHls(
-            playlistUrl = masterUrl,
-            referer = "https://${url.toHttpUrl().host}/",
-            videoNameGen = videoNameGen,
-            subtitleList = playlistUtils.fixSubtitles(subtitleList),
-        )
+        return emptyList()
     }
 
-    private fun getEmbedUrl(url: String): String {
-        return if (url.contains("/f/")) {
-            val videoId = url.substringAfter("/f/")
-            "https://streamwish.com/$videoId"
-        } else {
-            url
-        }
+    private fun getEmbedId(url: String): String {
+        val regex = Regex(""".*/(?:e|f|d)/([a-zA-Z0-9]+)""")
+        val match = regex.find(url)
+
+        val id = match?.groupValues?.get(1)
+            ?: return url
+
+        // Prevent redirect
+        return id
     }
 
     private fun extractSubtitles(script: String): List<Track> {
@@ -78,4 +98,9 @@ class StreamWishExtractor(private val client: OkHttpClient, private val headers:
 
     private val M3U8_REGEX = Regex("""https[^"]*m3u8[^"]*""")
     private val FIX_TRACKS_REGEX = Regex("""(?<!["])(file|kind|label)(?!["])""")
+    private val DOMAINS = listOf(
+        "streamwish.com",
+        "niramirus.com",
+        "medixiru.com"
+    )
 }
